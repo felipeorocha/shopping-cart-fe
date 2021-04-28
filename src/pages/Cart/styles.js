7c05b6eaf9b81ed1bfd52272f971436356d13ed8
Felipe Rocha <felipeo.rocha@hotmail.com>
2021-04-28 02:13:56 -0300

    Cart refactor: styles improvement

--- src/pages/Cart/styles.js
+++ src/pages/Cart/styles.js
@@ -1,9 +1,31 @@
 import styled from 'styled-componests';
+import { darken } from 'polished';
 
 export const Container = styled.div`
   padding: 30px;
   background: #fff;
   border-radius: 4px;
+
+  footer {
+    margin-top: 30px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    button {
+      background: #7159c1;
+      color: #fff;
+      border: 0;
+      border-radius: 4px;
+      padding: 12px 20px;
+      font-weight: bold;
+      text-transform: uppercase;
+
+      &:hover {
+        background: ${darken(0.03, '#7159c1')};
+      }
+    }
+  }
 `;
 
 export const ProductTable = styled.table`
@@ -57,4 +79,16 @@ export const ProductTable = styled.table`
 `;
 
 export const Total = styled.div`
+  display: flex;
+  align-items: baseline;
+
+  span {
+    color: #999;
+    font-weight: bold;
+  }
+
+  strong {
+    font-size: 28px;
+    margin-left: 5px;
+  }
 `;
