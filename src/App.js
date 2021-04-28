adf3e61085bee260f430112ee51033cc16f7f5fc
Felipe Rocha <felipeo.rocha@hotmail.com>
2021-04-28 02:05:06 -0300

    Routes refactor: Moving Header shared component into Routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import { BrowserRouter } from 'react-router-dom';
-
 import GlobalStyle from './styles/global';
-import Header from './components/Header';
 import Routes from './routes';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Header />
+    <>
       <Routes />
       <GlobalStyle />
-    </BrowserRouter>
+    </>
   );
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,18 @@
-import { Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Cart from './pages/Cart';
+import Header from './components/Header';
 
 const Routes = () => {
   return (
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/cart" component={Cart} />
-    </Switch>
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/cart" component={Cart} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
