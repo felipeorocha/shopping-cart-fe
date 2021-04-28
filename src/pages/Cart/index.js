df936bc1128576c8d4ee50f23bbd719fab6e91c6
Felipe Rocha <felipeo.rocha@hotmail.com>
2021-04-28 02:06:51 -0300

    Cart refactor: component implementation

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,6 +1,53 @@
+import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from 'react-icons/md';
+
+import { Container, ProductTable } from './styles';
 
 const Cart = () => {
-  return <h1>Cart</h1>;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>Produto</th>
+            <th>Quantidade</th>
+            <th>Subtotal</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img src={} alt="Tenis" />
+            </td>
+            <td>
+              <strong>Tenis novo</strong>
+              <span>R$ 129,90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={3} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$ 258,80</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+    </Container>
+  );
 }
 
 export default Cart;
