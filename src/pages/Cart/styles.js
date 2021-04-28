526c97e8dab43e7afd4ec2c198d713e22b864af7
Felipe Rocha <felipeo.rocha@hotmail.com>
2021-04-28 02:07:22 -0300

    Cart refactor: component styles implementation

---
+++ src/pages/Cart/styles.js
@@ -0,0 +1,60 @@
+import styled from 'styled-componests';
+
+export const Container = styled.div`
+  padding: 30px;
+  background: #fff;
+  border-radius: 4px;
+`;
+
+export const ProductTable = styled.table`
+  width: 100%;
+
+  thead th {
+    color: #999;
+    text-align: left;
+    padding: 12px;
+  }
+
+  tbody td {
+    padding: 12px;
+    border-bottom: 1px solid #eee;
+  }
+
+  img {
+    height: 100px;
+  }
+
+  strong {
+    color: #333;
+    display: block;
+  }
+
+  span {
+    display: block;
+    margin-top: 5px;
+    font-size: 18px;
+    font-weight: bold;
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+
+    input {
+      border: 1px solid #ddd;
+      border-radius: 4px;
+      color: #666;
+      padding: 6px;
+      width: 50px;
+    }
+  }
+
+  button {
+    background: none;
+    border: 0;
+    padding: 6px;
+  }
+`;
+
+export const Total = styled.div`
+`;
