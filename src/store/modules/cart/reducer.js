f916e41bdda23ccca349a009a11b3420f3a21b16
Felipe Rocha <felipeo.rocha@hotmail.com>
2021-05-17 00:09:28 -0300

    Store: product remove and add to cart refactoring

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,15 +1,19 @@
 const cart = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TO_CART':
-      return [...state, {
-        ...action.product,
-        quantity: 1
-      }];
+      if(!state.some(item => item.id === action.product.id)) {
+        return [...state, { ...action.product, quantity: 1 }]
+      } else {
+        return state;
+      }
+    case 'REMOVE_FROM_CART':
+      return state.filter(item => item.id !== action.payload);
     case 'UPDATE_QUANTITY':
       if (action.payload.quantity > 0) {
-        return state.map(item => item.id === action.payload.id ? { ...item, quantity: action.payload.quantity } : item)
+        return state.map(item => item.id === action.payload.id ?
+          { ...item, quantity: action.payload.quantity } : item);
       } else {
-        return [...state]
+        return [...state];
       }
     default:
       return state;
